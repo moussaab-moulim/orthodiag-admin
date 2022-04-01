87b66fb271d19fe2665fa43110fe1d6a801a8d29
moussaab moulim <moussaabmma@gmail.com>
2022-04-01 16:17:15 +0000

    hocs

--- src/hocs/with-auth-guard.tsx
+++ src/hocs/with-auth-guard.tsx
@@ -1,6 +1,11 @@
+import type { FC } from 'react';
 import { AuthGuard } from '../components/authentication/auth-guard';
 
-export const withAuthGuard = (Component) => (props) => (
+/**
+ * @deprecated Use the layout strategy
+ * @param Component
+ */
+export const withAuthGuard = (Component: FC) => (props: any) => (
   <AuthGuard>
     <Component {...props} />
   </AuthGuard>
--- src/hocs/with-guest-guard.tsx
+++ src/hocs/with-guest-guard.tsx
@@ -1,6 +1,11 @@
+import type { FC } from 'react';
 import { GuestGuard } from '../components/authentication/guest-guard';
 
-export const withGuestGuard = (Component) => (props) => (
+/**
+ * @deprecated Use the layout strategy
+ * @param Component
+ */
+export const withGuestGuard = (Component: FC) => (props: any) => (
   <GuestGuard>
     <Component {...props} />
   </GuestGuard>
